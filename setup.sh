346f9bcc68248165b0ad86f885cd350c67902617
matt-ntt-data <matthew.taylor@nttdata.com>
2015-08-13 10:51:01 +0100

    updated script

--- setup.sh
+++ setup.sh
@@ -3,9 +3,9 @@
 chown -R root:root /opt/eclipse
 chmod -R +r /opt/eclipse
 
-echo '#!/bin/sh\nexport ECLIPSE_HOME="/opt/eclipse"\n$ECLIPSE_HOME/eclipse $*' > /usr/bin/eclipse
+echo $'#!/bin/sh\nexport ECLIPSE_HOME="/opt/eclipse"\n$ECLIPSE_HOME/eclipse $*' > /usr/bin/eclipse
 
 chmod 755 /usr/bin/eclipse
 
-echo '[Desktop Entry]\nEncoding=UTF-8\nName=Eclipse\nComment=Eclipse IDE\nExec=eclipse\nIcon=/opt/eclipse/icon.xpm\nTerminal=false\nType=Application\nCategories=GNOME;Application;Development;\nStartupNotify=true\n' > eclipse.desktop
+echo $'[Desktop Entry]\nEncoding=UTF-8\nName=Eclipse\nComment=Eclipse IDE\nExec=eclipse\nIcon=/opt/eclipse/icon.xpm\nTerminal=false\nType=Application\nCategories=GNOME;Application;Development;\nStartupNotify=true\n' > eclipse.desktop
 
